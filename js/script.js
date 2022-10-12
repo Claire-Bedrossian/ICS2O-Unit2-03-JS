62deb364d20a07039ac127efb5bebfd188565c1b
Claire-Bedrossi <113055648+Claire-Bedrossian@users.noreply.github.com>
2022-10-12 18:48:21 +0000

    added function

---
+++ js/script.js
@@ -0,0 +1,18 @@
+// Copyright (c) 2022 Claire Bedrossian All rights reserved
+//
+// Created by: Claire Bedrossian
+// Created on: Sep 2022
+// This file contains the JS functions for index.html
+
+/**
+ * This function gets users street name and house number and shows it back to user.
+ */
+function myButtonClicked() {
+  // input
+  const streetName = document.getElementById("street-name").value
+  const houseNumber = parseInt(document.getElementById("house-number").value)
+
+  // output
+  document.getElementById("address").innerHTML =
+    "You live on: " + streetName + "  " + houseNumber + "."
+}
